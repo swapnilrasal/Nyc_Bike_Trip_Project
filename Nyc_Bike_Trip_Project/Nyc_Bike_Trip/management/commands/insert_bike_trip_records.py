e879eee432eeafc5a1d9005cbab4ed1abc1ad4e8
swapnilrasal <swapnilrasal1997@gmail.com>
2022-02-26 02:08:10 +0530

    modified insertion script

--- Nyc_Bike_Trip_Project/Nyc_Bike_Trip/management/commands/insert_bike_trip_records.py
+++ Nyc_Bike_Trip_Project/Nyc_Bike_Trip/management/commands/insert_bike_trip_records.py
@@ -1,13 +1,12 @@
-import csv
+# import csv
 import os
 from django.apps import apps
 from django.core.management.base import BaseCommand, CommandError
 from Nyc_Bike_Trip.models import BikeTrip
-# import requests
-from zipfile import ZipFile
 import wget
-
-# from reports.models import UNOOfficeReport, ACLandOfficeReport
+# import re
+import pandas as pd
+import zipfile
 
 
 class Command(BaseCommand):
@@ -21,6 +20,7 @@ class Command(BaseCommand):
     def insert_bike_trip_data_to_db(self, objs):
         try:
             self.stdout.write(self.style.SUCCESS('Inserting Data into BikeTrip Table'))
+            self.model_name.objects.all().delete()
             msg = self.model_name.objects.bulk_create(objs)
 
         except Exception as e:
@@ -28,34 +28,33 @@ class Command(BaseCommand):
                 self.model_name, str(e)))
 
     def download_zip_file(self, filename):
+        # import pdb
+        # pdb.set_trace()
         url = "https://s3.amazonaws.com/tripdata/"
         file_path = os.path.join(url, filename)
         current_path = os.path.dirname(os.path.abspath(__file__)).split('/')[-1]
-
-        req = wget.download(file_path,out =current_path)
+        print("\n")
+        req = wget.download(file_path, out=current_path)
         # # Split file_path to get the file name
         zip_filename = file_path.split('/')[-1]
-        csv_filename = zip_filename.replace(".zip",".csv")
+
         zip_file_path = current_path+f'\{{fname}}'.format(fname=zip_filename)
-        print(zip_file_path)
-
+        print("\n")
         self.stdout.write(self.style.SUCCESS('Extracting:{}'.format(filename)))
-        with ZipFile(zip_file_path, 'r') as zipObj:
-        # Get a list of all archived file names from the zip
-            listOfFileNames = zipObj.namelist()
-            # print(listOfFileNames)
-            # Iterate over the file names
-            for f_name in listOfFileNames:
-                # Check filename endswith csv
-                if f_name == csv_filename:
-                    # Extract a single file from zip
-                    zipObj.extract(f_name, current_path)
+        print("\n")
+        self.stdout.write(self.style.SUCCESS('Fetching Data From Zip File :{}'.format(filename)))
+        zf = zipfile.ZipFile(zip_file_path) 
+        df = pd.read_csv(zf.open(zipfile.ZipFile.namelist(zf)[0]))
+        total_rows = df.shape[0]
+        print("\n")
+        self.stdout.write(self.style.SUCCESS('Total Records :{}'.format(total_rows)))
+        print("\n") 
+        resp = {}
+        resp["df"] = df
+        resp["zip_file_path"] = zip_file_path
 
-        if os.path.exists(zip_file_path):
-            self.stdout.write(self.style.SUCCESS('Deleting:{}'.format(filename)))
-            os.remove(zip_file_path)
-
-        return current_path+f'\{{fname}}'.format(fname=csv_filename)
+        # print(df)
+        return resp
 
     def add_arguments(self, parser):
         parser.add_argument('filenames',
@@ -66,29 +65,41 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         for filename in options['filenames']:
+            print("*********************************************************************************************")
             self.stdout.write(self.style.SUCCESS('Downloading:{}'.format(filename)))
-            file_path = self.download_zip_file(filename)
-
+            resp = self.download_zip_file(filename)
+            # import pdb
+            # pdb.set_trace()
             try:
-                with open(file_path) as csv_file:
-                    csv_reader = csv.DictReader(csv_file)
-                    list_of_dict = list(csv_reader)
-                    objs = [
-                        BikeTrip(
-                            tripduration=row['tripduration'],
-                            starttime=row['starttime'],
-                            stoptime=row['stoptime'],                            
-                        )
-                        for row in list_of_dict
-                    ]
-                    self.insert_bike_trip_data_to_db(objs)
+                objs = [
+                    BikeTrip(
+                        trip_duration=(row[0] if not pd.isnull(row[0]) else None),
+                        start_time=(row[1] if not pd.isnull(row[1]) else None),
+                        stop_time=(row[2] if not pd.isnull(row[2]) else None),
+                        start_station_id=(row[3] if not pd.isnull(row[3]) else None),
+                        start_station_name=(row[4] if not pd.isnull(row[4]) else None),
+                        start_station_latitude=(row[5] if not pd.isnull(row[5]) else None),
+                        start_station_longitude=(row[6] if not pd.isnull(row[6]) else None),
+                        end_station_id=(row[7] if not pd.isnull(row[7]) else None),
+                        end_station_name=(row[8] if not pd.isnull(row[8]) else None),
+                        end_station_latitude=(row[9] if not pd.isnull(row[9]) else None),
+                        end_station_longitude=(row[10] if not pd.isnull(row[10]) else None),
+                        bikeid=(row[11] if not pd.isnull(row[11]) else None),
+                        user_type=(row[12] if not pd.isnull(row[12]) else None),                            
+                        birth_year=(row[13] if not pd.isnull(row[13]) else None),
+                        gender=(row[14] if not pd.isnull(row[14]) else None),
+                    )
+                    for index, row in resp["df"].iterrows()
+                ]
+                self.insert_bike_trip_data_to_db(objs)
 
-                if os.path.exists(file_path):
-                    self.stdout.write(self.style.SUCCESS('Deleting:{}'.format(file_path)))
-                    os.remove(file_path)
-        
+                if os.path.exists(resp["zip_file_path"]):
+                    print("\n")
+                    self.stdout.write(self.style.SUCCESS('Deleting:{}'.format(resp["zip_file_path"])))
+                    os.remove(resp["zip_file_path"])
+
+                print("\n")
                 self.stdout.write(self.style.SUCCESS('Data Inserted Successfully'))
-
+                print("\n*********************************************************************************************")
             except FileNotFoundError:
-                raise CommandError("File {} does not exist".format(
-                    file_path))
+                raise CommandError("File {} does not exist")
--- Nyc_Bike_Trip_Project/Nyc_Bike_Trip/models.py
+++ Nyc_Bike_Trip_Project/Nyc_Bike_Trip/models.py
@@ -1,6 +1,29 @@
 from django.db import models
+import datetime
+from django.utils.translation import gettext_lazy as _
+
 
 class BikeTrip(models.Model):
-    tripduration = models.IntegerField()
-    starttime = models.DateTimeField(auto_now_add=True)
-    stoptime = models.DateTimeField(auto_now_add=True)
+    genderChoice = (
+            (0,"unknown"),(1,"male"),(2,"Female")
+            )
+    userTypeChoice = (
+            ("Subscriber","Annual Member"),("Customer","24-hour pass or 3-day pass user")
+            )
+    # YEAR_CHOICES = [(r,r) for r in range(1984, datetime.date.today().year+1)]
+
+    trip_duration = models.IntegerField(null=True, blank=True)
+    start_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    stop_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    start_station_id = models.IntegerField(null=True, blank=True)
+    start_station_name = models.CharField(max_length=100, null=True, blank=True)
+    start_station_latitude = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    start_station_longitude = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    end_station_id = models.IntegerField(null=True, blank=True)
+    end_station_name = models.CharField(max_length=100, null=True, blank=True)
+    end_station_latitude = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    end_station_longitude = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    bikeid = models.IntegerField(null=True, blank=True)
+    gender = models.CharField(max_length = 1,choices=genderChoice, null=True, blank=True)
+    user_type = models.CharField(max_length = 50,choices=userTypeChoice, null=True, blank=True)
+    birth_year = models.PositiveSmallIntegerField(null=True, blank=True)
